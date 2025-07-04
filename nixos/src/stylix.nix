9a0adf80853fd7d921913156efaa323b6a6b1bad
makufff <Tanapatx.tyran@gmail.com>
2025-07-04 23:54:28 +0700

    change : boot wallpaper

--- nixos/src/stylix.nix
+++ nixos/src/stylix.nix
@@ -7,9 +7,9 @@
 	#image = "${assets/background/MoriCalliope.jpg}";
 	#image = "${assets/background/halloween.png}";
 	#image = "${assets/background/snow-miku.jpg}";
-	#image = "${assets/background/Alya.png}";
+	image = "${assets/background/Alya.png}";
 	#image = "${assets/background/anime-girl-with-terminal.jpg}";
-	image = "${assets/background/cyberpunk.jpg}";
+	# image = "${assets/background/cyberpunk.jpg}";
 	#image = "${assets/background/hutao1.jpg}";
 	#base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
 	#base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
