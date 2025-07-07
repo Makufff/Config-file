b3f318ca08a4942b7482b9d83daf30000ec39bb3
makufff <Tanapatx.tyran@gmail.com>
2025-07-07 14:51:10 +0700

    networking

--- nixos/src/networking.nix
+++ nixos/src/networking.nix
@@ -1,44 +1,21 @@
 { ... }:
+
 {
   networking = {
-    # ปิด DNS Extension Mechanism ของ resolvconf
-    resolvconf.dnsExtensionMechanism = false;
-
-    # ตั้ง DNS ถาวร
-    nameservers = [ "1.1.1.1" "8.8.8.8" ];
-
     # ใช้ NetworkManager
-    networkmanager = {
-      enable = true;
-      dns = "none"; # ปิดรับ DNS จาก DHCP
+    networkmanager.enable = true;
 
-      # เพิ่ม config บังคับ connection ใช้ DNS ที่ตั้งเอง
-      connectionConfig = ''
-        [ipv4]
-        method=auto
-        ignore-auto-dns=true
-        dns=1.1.1.1;8.8.8.8;
+    # ไม่ตั้ง nameservers เอง ใช้ DNS จาก DHCP
+    # networking.nameservers = []; # ไม่ต้องใส่
 
-        [ipv6]
-        method=auto
-        ignore-auto-dns=true
-      '';
-    };
+    # Firewall เปิดพอร์ตที่จำเป็น
+    firewall.enable = true;
+    firewall.allowedTCPPorts = [ 22 53 67 80 443 ];
+    firewall.allowedUDPPorts = [ 22 53 67 80 443 ];
+    firewall.allowedTCPPortRanges = [{ from = 1000; to = 65535; }];
+    firewall.allowedUDPPortRanges = [{ from = 1000; to = 65535; }];
 
-    # เปิด Firewall + ตั้งพอร์ต
-    firewall = {
-      enable = true;
-
-      allowedTCPPorts = [ 22 53 67 80 443 ];
-      allowedUDPPorts = [ 22 53 67 80 443 ];
-
-      allowedTCPPortRanges = [
-        { from = 1000; to = 65535; }
-      ];
-
-      allowedUDPPortRanges = [
-        { from = 1000; to = 65535; }
-      ];
-    };
+    # ตั้ง hostname ถ้าต้องการ
+    hostName = "nixos";
   };
 }
