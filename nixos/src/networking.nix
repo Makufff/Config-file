33a476f4b3b04bd68f00f95014fd4aca19cbdaa2
makufff <Tanapatx.tyran@gmail.com>
2025-07-07 15:08:08 +0700

    network

--- nixos/src/networking.nix
+++ nixos/src/networking.nix
@@ -1,30 +1,26 @@
-{ config, pkgs, ... }:
-
 {
+  ...
+} : {
+  networking.resolvconf.dnsExtensionMechanism = false;
   networking = {
-    resolvconf.dnsExtensionMechanism = false;
-
-    nameservers = [ "10.110.192.1" ];
 
     networkmanager = {
       enable = true;
-      dns = "none";
-
-      settings = {
-        main.dns = "none";
-        ipv4."ignore-auto-dns" = true;
-        ipv6."ignore-auto-dns" = true;
-      };
     };
 
     firewall = {
-      enable = true;
-
       allowedTCPPorts = [ 22 53 67 80 443 ];
       allowedUDPPorts = [ 22 53 67 80 443 ];
+      allowedTCPPortRanges = [
+        { from = 1000; to = 65535; }
+    ];
 
-      allowedTCPPortRanges = [ { from = 1000; to = 65535; } ];
-      allowedUDPPortRanges = [ { from = 1000; to = 65535; } ];
-    };
+     allowedUDPPortRanges = [
+       { from = 1000; to = 65535; }
+     ];
+
+     enable = true;
+    
+	};
   };
 }
