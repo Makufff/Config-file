27d84ba53c66236d62d9cf2e9936e8168dc0329a
makufff <Tanapatx.tyran@gmail.com>
2025-07-07 14:47:12 +0700

    networking

--- nixos/src/networking.nix
+++ nixos/src/networking.nix
@@ -1,23 +1,44 @@
+{ ... }:
 {
-  ...
-} : {
-  networking.resolvconf.dnsExtensionMechanism = false;
   networking = {
+    # ปิด DNS Extension Mechanism ของ resolvconf
+    resolvconf.dnsExtensionMechanism = false;
 
+    # ตั้ง DNS ถาวร
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
+    # ใช้ NetworkManager
     networkmanager = {
       enable = true;
+      dns = "none"; # ปิดรับ DNS จาก DHCP
+
+      # เพิ่ม config บังคับ connection ใช้ DNS ที่ตั้งเอง
+      connectionConfig = ''
+        [ipv4]
+        method=auto
+        ignore-auto-dns=true
+        dns=1.1.1.1;8.8.8.8;
+
+        [ipv6]
+        method=auto
+        ignore-auto-dns=true
+      '';
     };
 
+    # เปิด Firewall + ตั้งพอร์ต
     firewall = {
+      enable = true;
+
       allowedTCPPorts = [ 22 53 67 80 443 ];
       allowedUDPPorts = [ 22 53 67 80 443 ];
+
       allowedTCPPortRanges = [
         { from = 1000; to = 65535; }
-    ];
-     allowedUDPPortRanges = [
-       { from = 1000; to = 65535; }
-     ];
-      enable = true;
+      ];
+
+      allowedUDPPortRanges = [
+        { from = 1000; to = 65535; }
+      ];
     };
   };
 }
