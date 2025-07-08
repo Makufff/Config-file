19416b1919c7e4170a15a0039f0046b3a2176d4f
makufff <Tanapatx.tyran@gmail.com>
2025-07-09 01:43:47 +0700

    patch cisco

--- pkgs/packettracer/default.nix
+++ pkgs/packettracer/default.nix
@@ -1,9 +1,49 @@
-{ pkgs, ... }:
+{ lib
+, stdenv
+, dpkg
+, python3
+}:
 
-{
-  # NOTE: MANUAL INSTALL REQUIRED FOR PACKET TRACER:
-  # log into netacad and download packet tracer for ubuntu: https://www.netacad.com/resources/lab-downloads
-  # rename downloaded file to old schema: mv Packet_Tracer822_amd64_signed.deb CiscoPacketTracer822_amd64_signed.deb
-  # add it to the nix store: nix-store --add-fixed sha256 CiscoPacketTracer822_amd64_signed.deb
-  hm.home.packages = [ pkgs.ciscoPacketTracer8 ];
+stdenv.mkDerivation rec {
+  pname = "cisco-packet-tracer";
+  version = "8.2.2";
+
+  # ให้ใส่ path nix store ที่ได้จาก nix-store --add-fixed
+  src = /nix/store/6hjgf7b5vg9nqa4hl150pxdcs8xf4i15-CiscoPacketTracer822_amd64_signed.deb;
+
+  nativeBuildInputs = [ dpkg python3 ];
+
+  unpackPhase = ''
+    mkdir unpack
+    dpkg-deb -x $src unpack
+    dpkg-deb --control $src unpack/DEBIAN
+  '';
+
+  buildPhase = ''
+    python3 - <<EOF
+import os
+
+FIRST_JNZ_CHECK = 0x22265cb + 1
+SECOND_JNZ_CHECK = 0x2226920 + 1
+
+binary_path = "unpack/usr/local/bin/PacketTracer"
+
+with open(binary_path, "r+b") as f:
+    f.seek(FIRST_JNZ_CHECK)
+    f.write(b"\\x84")
+    f.seek(SECOND_JNZ_CHECK)
+    f.write(b"\\x84")
+EOF
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r unpack/* $out/
+  '';
+
+  meta = with lib; {
+    description = "Cisco Packet Tracer ${version} patched";
+    platforms = [ "x86_64-linux" ];
+    license = lib.licenses.free;
+  };
 }
