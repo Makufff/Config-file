caa66d5388d89db01e8b94c85fe8421cf1e5e94d
makufff <Tanapatx.tyran@gmail.com>
2025-07-08 18:17:24 +0700

    add : ppacket tracer by jarukit

--- pkgs/packettracer/default.nix
+++ pkgs/packettracer/default.nix
@@ -1,10 +1,13 @@
-{ lib, stdenv, dpkg }:
+{ lib, stdenv, dpkg, fetchurl }:
 
 stdenv.mkDerivation {
   pname = "packettracer";
   version = "8.2.2";
 
-  src = ./sources/Packet_Tracer822_amd64_signed.deb;
+  src = fetchurl {
+    url = "https://jarukrit.net/files/KMITL/Packet_Tracer822_amd64_signed.deb";
+    sha256 = "0bgplyi50m0dp1gfjgsgbh4dx2f01x44gp3gifnjqbgr3n4vilkc";
+  };
 
   buildInputs = [ dpkg ];
 
