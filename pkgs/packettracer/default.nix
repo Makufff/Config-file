434b551835b48d6df99d9be13a4ca9b2e0de1da3
makufff <Tanapatx.tyran@gmail.com>
2025-07-08 17:45:46 +0700

    feat : cisco-packet-tracer

--- pkgs/packettracer/default.nix
+++ pkgs/packettracer/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   pname = "packettracer";
   version = "8.2.2";
 
-  src = /nix/store/6562jz9mm4cika220bjxhbh42chp33hr-Packet_Tracer822_amd64_signed.deb;
+  src = ./source/Packet_Tracer822_amd64_signed.deb;
 
   unpackPhase = "true";
 
