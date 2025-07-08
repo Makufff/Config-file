94e5d4e00022448eee0d0ee365f06fc4b70849e7
makufff <Tanapatx.tyran@gmail.com>
2025-07-08 17:36:21 +0700

    feat : cisco-packet-tracer

---
+++ pkgs/cisco-packet-tracer/default.nix
@@ -0,0 +1,22 @@
+{ lib, stdenv }:
+
+stdenv.mkDerivation rec {
+  pname = "packettracer";
+  version = "8.2.2";
+
+  src = /nix/store/6562jz9mm4cika220bjxhbh42chp33hr-Packet_Tracer822_amd64_signed.deb;
+
+  unpackPhase = "true";
+
+  installPhase = ''
+    mkdir -p $out
+    dpkg -x $src $out
+  '';
+
+  meta = {
+    description = "Cisco Packet Tracer";
+    homepage = "https://www.netacad.com";
+    license = lib.licenses.unfree;
+    platforms = lib.platforms.linux;
+  };
+}
