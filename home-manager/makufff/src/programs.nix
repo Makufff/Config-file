7853989fdcb6eeceb0e6bf308b8406b969ce759c
makufff <Tanapatx.tyran@gmail.com>
2025-07-18 18:07:19 +0700

    change neovim to lazyvim

--- home-manager/makufff/src/programs.nix
+++ home-manager/makufff/src/programs.nix
@@ -43,161 +43,41 @@
       unfocused-split-opacity = 0.7;
     };
   };
-	programs.neovim =
-	let
-		toLua = str: "lua << EOF\n${str}\nEOF\n";
-	in
-	{
-		enable = true;
-		defaultEditor = true;
-		viAlias = true;
-		vimAlias = true;
-		vimdiffAlias = true;
-		plugins = with pkgs.vimPlugins; [
-			transparent-nvim
-			{
-				plugin = telescope-nvim;
-				config = toLua ''
-					local builtin = require('telescope.builtin')
-					require('telescope').setup{
-						defaults = {
-							-- Default configuration for telescope goes here:
-							-- config_key = value,
-							mappings = {
-								i = {
-									-- map actions.which_key to <C-h> (default: <C-/>)
-									-- actions.which_key shows the mappings for your picker,
-									-- e.g. git_{create, delete, ...}_branch for the git_branches picker
-									["<C-h>"] = "which_key"
-								}
-							}
-						},
-						pickers = {},
-						extensions = {}
-					}
-					vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-					vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-					vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-					vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-				'';
-			}
-			vim-airline {
-				plugin = vim-airline-themes;
-				config = ''
-					let g:airline_powerline_fonts = 1
-					let g:airline_theme = 'base16'
-				'';
-			}
-			{
-				plugin = comment-nvim;
-				config = toLua ''
-					require("Comment").setup {}
-				'';
-			}
-			nvim-lspconfig mason-nvim {
-				plugin = mason-lspconfig-nvim;
-				config = toLua ''
-					require("mason").setup()
-					require("mason-lspconfig").setup {
-						ensure_installed = {
-							"nil_ls",
-							"clangd",
-							"rust_analyzer"
-						}
-					}
-					require("lspconfig").nil_ls.setup {
-						cmd = {"/run/current-system/sw/bin/nixd";}
-					}
-					require("lspconfig").clangd.setup {
-						cmd = {"/run/current-system/sw/bin/clangd"},
-					}
-					require("lspconfig").rust_analyzer.setup {}
-				'';
-			}
-			cmp-nvim-lsp cmp-buffer cmp-path cmp-cmdline {
-				plugin = nvim-cmp;
-				config = toLua ''
-					local cmp = require('cmp');
-					cmp.setup {
-						mapping = cmp.mapping.preset.insert({
-							['<C-b>'] = cmp.mapping.scroll_docs(-4),
-							['<C-f>'] = cmp.mapping.scroll_docs(4),
-							['<C-Space>'] = cmp.mapping.complete(),
-							['<C-e>'] = cmp.mapping.abort(),
-							['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-						}),
-						sources = {
-							{ name = 'nvim_lsp' },
-							{ name = 'buffer' },
-							{ name = 'rust_analyzer' },
-						}
-					}
-					cmp.setup.cmdline({ '/', '?' }, {
-						mapping = cmp.mapping.preset.cmdline(),
-							sources = {
-								{ name = 'buffer' }
-							}
-						})
-					cmp.setup.cmdline(':', {
-						mapping = cmp.mapping.preset.cmdline(),
-						sources = cmp.config.sources({
-							{ name = 'path' }
-						}, {
-							{ name = 'cmdline' }
-						}),
-						matching = { disallow_symbol_nonprefix_matching = false }
-					})
-				'';
-			}
-			{
-				plugin = (nvim-treesitter.withPlugins (p: [
-					p.tree-sitter-nix
-					p.tree-sitter-vim
-					p.tree-sitter-bash
-					p.tree-sitter-lua
-					p.tree-sitter-python
-					p.tree-sitter-json
-					p.tree-sitter-c
-					p.tree-sitter-rust
-				]));
-				config = toLua ''
-					require('nvim-treesitter.configs').setup {
-						ensure_installed = {},
-						auto_install = false,
-						highlight = { enable = true },
-						indent = { enable = true },
-					}
-				'';
-			}
-			{
-				plugin = lsp_signature-nvim;
-				config = toLua ''
-					local cfg = {}  -- add your config here
-					require "lsp_signature".setup(cfg)
-				'';
-			}
-			{
-				plugin = vim-clang-format;
-			}
-		];
-		extraConfig = ''
-			" set		autoindent
-			set			smartindent
-			set			noexpandtab
-			" set		expandtab
-			set			tabstop=4
-			set			shiftwidth=4
-			set			backspace=indent,eol,start
-			" syntax	on
-			set			nu
-			set			list
-			" set			listchars+=space:⋅
-			" set			listchars+=tab:→\
-			" set			listchars+=eol:↴
-			hi Pmenu		ctermfg=white ctermbg=black gui=NONE guifg=white guibg=black
-			hi PmenuSel		ctermfg=white ctermbg=blue gui=bold guifg=white guibg=purple
-		'';
-	};
+  # Use LazyVim for neovim, vim, and vi
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      lazy-nvim
+      LazyVim
+      # Add more plugins here if needed
+    ];
+    extraLuaConfig = ''
+      vim.g.mapleader = " "
+      require("lazy").setup({
+        spec = {
+          { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+          -- { import = "plugins" }, -- Uncomment if you have your own plugins
+          { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
+        },
+        dev = {
+          path = "${pkgs.linkFarm "lazy-plugins" (builtins.map (drv: { name = drv.pname or drv.name; path = drv; }) (with pkgs.vimPlugins; [ LazyVim ]))}",
+          patterns = { "" },
+          fallback = true,
+        },
+        install = { missing = false },
+        performance = {
+          reset_packpath = false,
+          rtp = { reset = false },
+        },
+      })
+    '';
+  };
+  # Optionally, symlink your own LazyVim config:
+  # xdg.configFile."nvim".source = ./dotfiles/nvim; # Uncomment and set path if you want to use your own config
 	programs.fastfetch = {
 		enable = true;
 		settings = {
