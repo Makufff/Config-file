1b5447e1b5ab85359ecad85f09ab69c92469d973
makufff <Tanapatx.tyran@gmail.com>
2025-07-18 17:33:33 +0700

    fix : custom ghostty

--- home-manager/makufff/src/programs.nix
+++ home-manager/makufff/src/programs.nix
@@ -41,25 +41,6 @@
       theme = "my-rainbow";
       background-opacity = 0.85;
       unfocused-split-opacity = 0.7;
-      keybind = [
-        "ctrl+shift+c=copy_to_clipboard"
-        "ctrl+shift+v=paste_from_clipboard"
-        "ctrl+shift+t=new_tab"
-        "ctrl+shift+w=close_tab"
-        "ctrl+shift+n=new_window"
-        "ctrl+shift+plus=increase_font_size"
-        "ctrl+shift+minus=decrease_font_size"
-        "ctrl+shift+0=reset_font_size"
-      ];
-      extraConfig = ''
-        padding-x=12
-        padding-y=8
-        cursor-shape=beam
-        cursor-color=ffb86c
-        bell=off
-        scrollback=5000
-        window-theme=dark
-      '';
     };
     themes = {
       my-rainbow = {
