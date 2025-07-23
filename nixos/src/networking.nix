abb8f2b9de3a654d499f3f3407faf428c1ce6a51
makufff <Tanapatx.tyran@gmail.com>
2025-07-24 01:12:49 +0700

    add : network

--- nixos/src/networking.nix
+++ nixos/src/networking.nix
@@ -3,24 +3,23 @@
 } : {
   networking.resolvconf.dnsExtensionMechanism = false;
   networking = {
-
+    nameservers = [ "8.8.8.8" "1.1.1.1" ];
     networkmanager = {
       enable = true;
+      dns = "none";
     };
-
     firewall = {
       allowedTCPPorts = [ 22 53 67 80 443 ];
       allowedUDPPorts = [ 22 53 67 80 443 ];
       allowedTCPPortRanges = [
         { from = 1000; to = 65535; }
     ];
-
      allowedUDPPortRanges = [
        { from = 1000; to = 65535; }
      ];
-
      enable = true;
-    
 	};
+    resolvconf.enable = false;
   };
+  services.resolved.enable = false;
 }
