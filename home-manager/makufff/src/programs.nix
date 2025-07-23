c6a6e20b41eb9b33524c495f83905931e0f85bd4
makufff <Tanapatx.tyran@gmail.com>
2025-07-24 00:14:03 +0700

    add capture screen in waybar

--- home-manager/makufff/src/programs.nix
+++ home-manager/makufff/src/programs.nix
@@ -17,14 +17,15 @@
 		enableCompletion = true;
 		shellAliases = {
 			neofetch = "fastfetch";
+			fixvpn = "sudo resolvectl dns tun0 10.30.32.1 && sudo resolvectl domain tun0 '~.'";
 		};
 		initContent = ''
 			function it-kmitl() {
 				case "$1" in
 					start)
-						sudo systemctl start openvpn-kmitl ;;
+						sudo systemctl start openvpn-kmitl && sudo resolvectl dns tun0 10.30.32.1 && sudo resolvectl domain tun0 '~.' ;;
 					stop)
-						sudo systemctl stop openvpn-kmitl ;;
+						sudo systemctl stop openvpn-kmitl && sudo resolvectl dns tun0 '' && sudo resolvectl domain tun0 '' ;;
 					status)
 						sudo systemctl status openvpn-kmitl ;;
 					*)
