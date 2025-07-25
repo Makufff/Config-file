c3ffaa9f12a35555a67d200e4859151dc5c4817d
makufff <Tanapatx.tyran@gmail.com>
2025-07-26 00:33:17 +0700

    add : softeater

--- nixos/src/environment.nix
+++ nixos/src/environment.nix
@@ -10,4 +10,22 @@
 		];
 		EXTRA_LDFLAGS="-L/lib -L${config.boot.kernelPackages.nvidia_x11}/lib";
 	};
+	
+	# Alternative: Initialize conda in all shells
+	environment.shellInit = ''
+		# >>> conda initialize >>>
+		# !! Contents within this block are managed by 'conda init' !!
+		__conda_setup="$('${pkgs.conda}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+		if [ $? -eq 0 ]; then
+			eval "$__conda_setup"
+		else
+			if [ -f "${pkgs.conda}/etc/profile.d/conda.sh" ]; then
+				. "${pkgs.conda}/etc/profile.d/conda.sh"
+			else
+				export PATH="${pkgs.conda}/bin:$PATH"
+			fi
+		fi
+		unset __conda_setup
+		# <<< conda initialize <<<
+	'';
 }
